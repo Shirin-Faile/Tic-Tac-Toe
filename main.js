449eabcadfcafae57da34b044082eb9de2e7d3e9
shirin faile <shirin.faile@hotmail.com>
2023-12-17 11:41:22 +0100

    assignment 3 improved

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 $(() => {
 
-    let currentPlayer, gameBoard, gameActive, turnCount, wins, losses;
+    let currentPlayer, gameBoard, gameActive, turnCount, wins, losses, isAIPlayer;
+    let isMuted = false;
 
     wins = { 'X': 0, 'O': 0};
     losses = { 'X': 0, 'O': 0};
@@ -26,7 +27,11 @@ $(() => {
     };
 
     const updateGameInfo = () => {
-        $('#player-info').text(`Player ${currentPlayer}'s turn`);
+        if (isAIPlayer && currentPlayer === 'O' && gameActive) {
+            makeAIMove();
+        } else {
+            $('#player-info').text(`Player ${currentPlayer}'s turn`);
+        }
     };
 
     const checkWin = () => {
@@ -73,26 +78,104 @@ $(() => {
         const index = $(event.currentTarget).data('index');
         if (gameBoard[index] === '' && gameActive) {
             gameBoard[index] = currentPlayer;
+            $(event.currentTarget).addClass('animate');
             $(event.currentTarget).text(currentPlayer);
             turnCount++;
+
+            playClickSound();
     
             if (!checkWin()) {
                 switchPlayer();
                 updateGameInfo();
             }
+
+            setTimeout(() => {
+                $(event.currentTarget).removeClass('animate');
+            }, 200);
+
         }
     };
+
+    const playClickSound = () => {
+        if (!isMuted) {
+            const clickSound = new Audio('./clickeffect/cork-85200.mp3');
+            clickSound.volume = 0.1;
+            clickSound.play();
+        }
+    }
 
     const switchPlayer = () => {
         currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
     };
 
     const updateScoreboard = () => {
-        $('#scoreboard').text(`Wins: X - ${wins['X']}, O - ${wins['O']} | Losses: X - ${losses['X']}, O - ${losses['O']}`);
+        const scoreboardElement = $('#scoreboard');
+        
+        const scoreboardText = `
+            <p>Wins: X - ${wins['X']}, O - ${wins['O']}</p>
+            <p>Losses: X - ${losses['X']}, O - ${losses['O']}</p>`;
+    
+        scoreboardElement.html(scoreboardText);
     };
 
     $('#replay-button').on('click', initializeGame);
 
-    initializeGame();
+    const toggleMute = () => {
+        isMuted = !isMuted;
+        const muteButton = $('#muteButton');
 
+        if (isMuted) {
+            muteButton.html('<img src="./clickeffect/icons8-no-audio-30.png" alt="muted">');
+        } else {
+            muteButton.html('<img src="./clickeffect/icons8-audio-30.png" alt="unmuted">');
+        }
+    };
+
+    const makeAIMove = () => {
+        const availableMoves = [];
+        for (let i = 0; i < gameBoard.length; i++) {
+            if (gameBoard[i] === '') {
+                availableMoves.push(i);
+            }
+        }
+        const randomMove = availableMoves[Math.floor(Math.random() * availableMoves.length)];
+        gameBoard[randomMove] = 'O';
+        $(`.square[data-index="${randomMove}"]`).addClass('animate');
+        $(`.square[data-index="${randomMove}"]`).text('O');
+        turnCount++;
+        playClickSound();
+
+        if (!checkWin()) {
+            switchPlayer();
+            updateGameInfo();
+        }
+        setTimeout(() => {
+            $(`.square[data-index="${randomMove}"]`).removeClass('animate');
+        }, 200);
+    };
+
+    const createAIButton = () => {
+        const aiButton = $('<button id="toggleAIButton"><img id="aiIcon" src="./clickeffect/two-players.png" alt="2-player"></button>');
+        
+        aiButton.on('click', () => {
+            isAIPlayer = !isAIPlayer;
+            updateAIButtonIcon();
+        });
+    
+        $('#player-info').after(aiButton);
+    };
+    
+    const updateAIButtonIcon = () => {
+        const aiIcon = $('#aiIcon');
+        const newIconSrc = isAIPlayer ? './clickeffect/ai.png' : './clickeffect/two-players.png';
+        
+        aiIcon.attr('src', newIconSrc);
+        aiIcon.attr('alt', isAIPlayer ? 'AI' : '2-player');
+    };
+    createAIButton();
+
+    $('#replay-button').on('click', initializeGame);
+    $('#muteButton').on('click', toggleMute);
+
+    initializeGame();
 });
